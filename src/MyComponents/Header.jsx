431297d3487257e69f3e35c1d71de87819618e07
Sidddanth Katipally <siddanth6365@gmail.com>
2023-04-26 23:35:05 +0530

    changed toggle bg

--- src/MyComponents/Header.jsx
+++ src/MyComponents/Header.jsx
@@ -145,7 +145,7 @@ const Header = () => {
               <li>
                 <a
                   href="#project-area"
-                  className="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-gray-400 rounded  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 "
                 >
                   Projects
                 </a>
@@ -153,7 +153,7 @@ const Header = () => {
               <li>
                 <a
                   href="#message-form"
-                  className="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white "
+                  className="block py-2 pl-3 pr-4 text-gray-400 rounded  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white "
                 >
                   Contact
                 </a>
